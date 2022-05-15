2116e0a6bb06ddb7f78fc4ddc83a8bd57b89bf82
Mustafa Allam <66793691+Mustafaallam20@users.noreply.github.com>
2022-05-15 23:48:25 +0200

    show schedule module

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ from app.controllers.Session.register import register_manage
 
 from app.controllers.tickets.ticket_manage import ticket_manage
 
+from app.controllers.trips.schedule import schedule_manage
+
 APP = Flask(__name__)
 APP.secret_key = 'secret'
 APP.register_blueprint(reserve_ticket)
 APP.register_blueprint(register_manage)
 APP.register_blueprint(ticket_manage)
+APP.register_blueprint(schedule_manage)
 
 if __name__ == "__main__":
     APP.run(debug=True)
